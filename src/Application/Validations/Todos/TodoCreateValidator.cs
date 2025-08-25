d6468990bf22c15981a85cc7a94742b73a131036
Abir <amabirmahmud@gmail.com>
2025-08-25 19:16:45 +0600

    Update TodoCreateValidator.cs

--- src/Application/Validations/Todos/TodoCreateValidator.cs
+++ src/Application/Validations/Todos/TodoCreateValidator.cs
@@ -9,8 +9,8 @@ public class TodoCreateValidator : AbstractValidator<TodoCreateViewModel>
     public TodoCreateValidator()
     {
         RuleFor(x => x).NotNull();
-        RuleFor(x => x.DueDateUtc).GreaterThanOrEqualTo(DateTime.UtcNow);
         RuleFor(x => x.Title).NotEmpty();
+        RuleFor(x => x.DueDateUtc).GreaterThanOrEqualTo(DateTime.UtcNow);
         RuleFor(x => x.Priority).NotEmpty().GreaterThan(0).LessThanOrEqualTo(5);
     }
 }
